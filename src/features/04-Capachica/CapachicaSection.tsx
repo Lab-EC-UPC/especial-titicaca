43816d06e4d78ea85bf31ad11c48061a370cc97a
Lucero <luceroabigail.or@gmail.com>
2026-05-15 21:55:46 -0500

    feat: Add subsection testimonials

--- src/features/04-Capachica/CapachicaSection.tsx
+++ src/features/04-Capachica/CapachicaSection.tsx
@@ -1,8 +1,9 @@
 import { LifecycleGallery } from "./components/lifecycle/LifecycleGallery";
-
+import { TestimonialGallery } from "./components/testimonials/TestimonialGallery";
 
 export const CapachicaSection = () => (
   <section id="capachica">
     <LifecycleGallery />
+    <TestimonialGallery />
   </section>
 );
