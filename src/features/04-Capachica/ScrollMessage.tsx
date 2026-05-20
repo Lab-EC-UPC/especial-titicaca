dd88995d331e2e9c8e4ef0c5ad09e017c9b55e0b
u202210494 <u202210494@upc.edu.pe>
2026-05-19 23:25:53 -0500

    Add some sections in Capachica

--- src/features/04-Capachica/ScrollMessage.tsx
+++ src/features/04-Capachica/ScrollMessage.tsx
@@ -26,6 +26,26 @@ export const ScrollMessage = ({ start, end, children }: ScrollMessageProps) => {
     );
 };
 
+ScrollMessage.Wide = ({ start, end, children }: ScrollMessageProps) => {
+    return (
+        <div
+            className="absolute inset-0 z-20 grid place-items-center px-4 opacity-0 pointer-events-none"
+            data-start={start}
+            data-end={end}
+        >
+            <div className="relative flex w-full max-w-[min(92vw,1200px)] items-center justify-center text-white">
+                <div
+                    aria-hidden="true"
+                    className="pointer-events-none absolute inset-[-3.5rem] -z-10 rounded-[999px] bg-black/28 blur-[52px] opacity-90"
+                />
+                <div className="relative z-10 max-w-full drop-shadow-[0_16px_26px_rgba(0,0,0,0.92)] [&_img]:block [&_img]:max-w-full [&_img]:rounded-[1.25rem]">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
 type ScrollParagraphProps = {
     children: ReactNode;
 };
