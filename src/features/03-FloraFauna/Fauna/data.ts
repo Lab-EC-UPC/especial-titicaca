c499023b60c6a7bfbb28a6d3b7cf9c590dbf6b94
Vilduis <luisvilders@gmail.com>
2026-05-20 19:07:42 -0500

    leg movement

---
+++ src/features/03-FloraFauna/Fauna/data.ts
@@ -0,0 +1,15 @@
+import Boga   from "../../../assets/BOGA.png";
+import Orestia from "../../../assets/Orestia-cuvieri.png";
+import Suche   from "../../../assets/SUCHE.png";
+
+export const avesInfo = [
+  { nombre: "Flamenco chileno",       cientifico: "Nombre cientifico", numero: 259,  estado: "Peligro menor" },
+  { nombre: "Parihuana negra",        cientifico: "Nombre cientifico", numero: 581,  estado: "Peligro medio" },
+  { nombre: "Zampullín del Titicaca", cientifico: "Nombre cientifico", numero: 1766, estado: "Peligro Alto"  },
+];
+
+export const pecesData = [
+  { nombre: "Boga",            cientifico: "Nombre cientifico", img: Boga,    numero: 120, estado: "Peligro menor" },
+  { nombre: "Orestia cuvieri", cientifico: "Nombre cientifico", img: Orestia, numero: 340, estado: "Peligro medio" },
+  { nombre: "Suche",           cientifico: "Nombre cientifico", img: Suche,   numero: 890, estado: "Peligro Alto"  },
+];
